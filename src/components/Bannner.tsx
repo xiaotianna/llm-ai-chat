2b53349da89f626574364a38974d0a403a369d35
wifi歪f <1402772884@qq.com>
2025-11-26 19:49:38 +0800

    fix: banner is mobile

--- src/components/Bannner.tsx
+++ src/components/Bannner.tsx
@@ -1,10 +1,13 @@
+"use client"
 import React from 'react'
 import { AlertCircle } from 'lucide-react'
 import { Banner, BannerClose, BannerTitle } from './ui/banner'
+import { useIsMobile } from '../hooks/use-mobile';
 
 const Bannner = () => {
+  const isMobile = useIsMobile()
   return (
-    <div className='w-full fixed top-0 z-50'>
+    isMobile && <div className='w-full fixed top-0 z-50'>
       <Banner>
         <AlertCircle className='h-5 w-5 text-blue-600 dark:text-blue-400' />
         <BannerTitle>请使用桌面端访问效果最佳～🎉</BannerTitle>
